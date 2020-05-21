008f63b59071573f984bf2cedbfcfa16bd18f9f2
Sweetysweet <gsm_v@mail.ru>
2020-05-22 00:14:47 +0300

    init

---
+++ GoSurfe/temp/symbols-data-template.js
@@ -0,0 +1,13 @@
+__iconsData: {
+    
+        'favicon': {
+            width: '512px',
+            height: '512px'
+        },
+    
+        'logo-pinkman': {
+            width: '69px',
+            height: '10px'
+        },
+    
+}
