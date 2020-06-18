6cf534611ec712113f202d797179e1ce984bf5a1
Sweetysweet <gsm_v@mail.ru>
2020-06-19 02:18:57 +0300

    del

--- Stolovay.ru/markup/components/header/form.js
+++
@@ -1,9 +0,0 @@
-const phoneBtn = document.querySelector('.menu__contact_phone'),
-    popUp = document.querySelector('.contact-popup'),
-    closeBtn = document.querySelector('.contact-popup__close');
-phoneBtn.addEventListener('click', () => {
-    popUp.style.display = 'none' ? 'block' : 'none';
-})
-closeBtn.addEventListener('click', () => {
-    popUp.style.display = 'none';
-})
--- Stolovay.ru/markup/static/js/helpers/is-in-viewport.js
+++
@@ -1,7 +0,0 @@
-export default function addIsInViewPort() {
-	$.fn.isInViewport = function () {
-		const $this = $(this);
-		const $window = $(window);
-		return ($this.offset().top + $this.outerHeight()) > $window.scrollTop() && $this.offset().top < ($window.scrollTop() + $window.height());
-	};
-}
