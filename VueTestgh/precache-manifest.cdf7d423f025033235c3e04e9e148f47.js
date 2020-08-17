4d1a0ffa8ccf34f73b8c1d564234d78792a140db
Sweetysweet <gsm_v@mail.ru>
2020-08-17 23:23:32 +0300

    git init

---
+++ VueTestgh/precache-manifest.cdf7d423f025033235c3e04e9e148f47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83914d77f96dce3d3c1c",
+    "url": "/testProject/css/app.4d08c71d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/testProject/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "420d455c99c12e1af605b51df38a0c80",
+    "url": "/testProject/index.html"
+  },
+  {
+    "revision": "6bae731744aa7626fee0",
+    "url": "/testProject/js/about.7cea5f74.js"
+  },
+  {
+    "revision": "83914d77f96dce3d3c1c",
+    "url": "/testProject/js/app.9ac80403.js"
+  },
+  {
+    "revision": "b7c30f17e999d5a94e3e",
+    "url": "/testProject/js/chunk-vendors.2e748bf0.js"
+  },
+  {
+    "revision": "4dd2751dfa44c648270334e74aa5aa10",
+    "url": "/testProject/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/testProject/robots.txt"
+  }
+]);
