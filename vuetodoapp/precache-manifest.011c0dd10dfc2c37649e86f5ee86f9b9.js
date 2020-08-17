d14c790073dc0cc2df3f54ae69317bba0f585728
Sweetysweet <gsm_v@mail.ru>
2020-08-17 23:45:49 +0300

    prod vueRunner

---
+++ vuetodoapp/precache-manifest.011c0dd10dfc2c37649e86f5ee86f9b9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27a7d459848d3951dfde",
+    "url": "/vuetodoapp/css/app.bda55a5f.css"
+  },
+  {
+    "revision": "02c11f774b7359c39c21",
+    "url": "/vuetodoapp/css/chunk-5e275a16.07590443.css"
+  },
+  {
+    "revision": "4fa7bf7c3584bf7fe190",
+    "url": "/vuetodoapp/css/chunk-vendors.3b8adcd8.css"
+  },
+  {
+    "revision": "3e2c0e53791247ba4a64280c04feca28",
+    "url": "/vuetodoapp/index.html"
+  },
+  {
+    "revision": "27a7d459848d3951dfde",
+    "url": "/vuetodoapp/js/app.d5433a4a.js"
+  },
+  {
+    "revision": "e98f3b0ae91342ef0c93",
+    "url": "/vuetodoapp/js/chunk-2d0b64c8.be438fde.js"
+  },
+  {
+    "revision": "24cf574a79ab35696280",
+    "url": "/vuetodoapp/js/chunk-2d21ef2c.742331e5.js"
+  },
+  {
+    "revision": "02c11f774b7359c39c21",
+    "url": "/vuetodoapp/js/chunk-5e275a16.0d33dd2f.js"
+  },
+  {
+    "revision": "4fa7bf7c3584bf7fe190",
+    "url": "/vuetodoapp/js/chunk-vendors.ab358805.js"
+  },
+  {
+    "revision": "ffd1c1b2d95fc404be4c6317bdc548b8",
+    "url": "/vuetodoapp/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vuetodoapp/robots.txt"
+  }
+]);
